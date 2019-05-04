df7308e490b78167fde61d4ae81e1bf26f1af4ca
Vikas Ranjan <vranjan@mobileiron.com>
2019-05-04 21:39:27 +0530

    Initialize account data

--- src/main/java/com/turvo/teller/atm/AtmApplication.java
+++ src/main/java/com/turvo/teller/atm/AtmApplication.java
@@ -1,5 +1,6 @@
 package com.turvo.teller.atm;
 
+import dbobject.LocalAccountDetails;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AtmApplication {
 
 	public static void main(String[] args) {
+	    // Initialize account data.
+        LocalAccountDetails.getInstance();
 		SpringApplication.run(AtmApplication.class, args);
 	}
 
